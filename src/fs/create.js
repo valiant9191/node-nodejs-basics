083242bcf8ef7e924a32a0ce3e11f1775a12f10f
Kolko Valentin <valiant9191@gmail.com>
2023-06-14 18:48:42 +0300

    feat: add function to create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { writeFile } from 'fs/promises';
+import path from 'path';
+
+
 const create = async () => {
-    // Write your code here 
+    const absolutePath = process.argv[1]
+    const folderPath = path.dirname(absolutePath) + '/files'
+    const data = 'I am fresh and young'
+    const errorMessage = 'FS operation failed'
+
+    try {
+        writeFile(`${folderPath}/fresh.txt`, data, { flag: 'wx' }, err => {
+            console.log(err)
+        }).then(() => console.log('done'))
+            .catch(err => {
+                throw new Error(errorMessage)
+            })
+    } catch (err) {
+        console.log(err)
+    }
 };
 
 await create();
