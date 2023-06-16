aa9933e6ca0e68a0676ee81162c9002bec206e65
Kolko Valentin <valiant9191@gmail.com>
2023-06-16 21:45:46 +0300

    feat: add func to list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { readdir } from 'fs/promises';
+import path from 'path'
+
 const list = async () => {
-    // Write your code here 
+    const pathAbsolute = path.dirname(process.argv[1])
+    const filesFolder = `${pathAbsolute}/files`
+    const errorMessage = 'FS operation failed'
+
+    try {
+        const files = await readdir(filesFolder)
+        console.log(files)
+
+    } catch (err) {
+        console.error(errorMessage)
+    }
 };
 
 await list();
