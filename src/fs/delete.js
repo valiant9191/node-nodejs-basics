4bab448b1c1c76c28fb4b9ec24b3a416cddbc831
Kolko Valentin <valiant9191@gmail.com>
2023-06-16 21:13:43 +0300

    feat: add delete func to delete task;

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,4 +1,18 @@
+import { rm } from 'fs/promises';
+import path from 'path';
+
+
 const remove = async () => {
+    const pathAbsolute = path.dirname(process.argv[1])
+    const errorMessage = 'FS operation failed'
+    const fileToDelete = `${pathAbsolute}/files/fileToRemove.txt`
+
+    try {
+        await rm(fileToDelete)
+            .then(() => console.log('successfully removed'))
+    } catch (error) {
+        console.error(errorMessage)
+    }
     // Write your code here 
 };
 
