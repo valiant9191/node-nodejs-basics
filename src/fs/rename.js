e53ef9b6a0df894c00aba28a57f51a5c5b5d2b9d
Kolko Valentin <valiant9191@gmail.com>
2023-06-16 20:56:21 +0300

    feat: add func to rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import { access, rename as renameFS } from 'fs/promises';
+import path from 'path';
+
+
+
 const rename = async () => {
-    // Write your code here 
+    const pathAbsolute = path.dirname(process.argv[1])
+    const fileToRename = `${pathAbsolute}/files/wrongFilename.txt`
+    const newFileName = `${pathAbsolute}/files/properFilename.md`
+    const errorMessage = 'FS operation failed'
+
+    try {
+        let fileExists = await access(newFileName)
+            .then(() => true)
+            .catch(() => false)
+
+        await renameFS(fileToRename, !fileExists ? newFileName : '')
+            .then(() => console.log('successfully renamed'))
+    } catch (error) {
+        console.error(errorMessage)
+    }
 };
 
 await rename();
