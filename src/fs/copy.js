0a7de5b43e16a902725db307d9a6721c7a3c6b34
Kolko Valentin <valiant9191@gmail.com>
2023-06-16 11:15:02 +0300

    feat: add func to copy-task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import { mkdir, access, readdir, cp } from 'fs/promises';
+import { basename, dirname } from 'path';
+
 const copy = async () => {
-    // Write your code here 
+    const pathAbsolute = process.argv[1];
+    const pathOriginal = dirname(pathAbsolute) + '/files'
+    const pathDestinationCopy = dirname(pathAbsolute) + '/files_copy'
+    const errorMessage = 'FS operation failed'
+
+    try {
+        // check folder with files exists 
+        await access(pathOriginal)
+        // create destination folder 
+        await mkdir(pathDestinationCopy)
+        // copy files in folder & check if folder exists
+        cp(pathOriginal, pathDestinationCopy, { recursive: true })
+            .then(() => console.log('successfully copied'))
+    } catch (error) {
+        console.error(errorMessage + '  ' + error.message)
+    }
 };
 
 await copy();
