2473e93955eaa147f9617fd07317b13e5752264a
Kolko Valentin <valiant9191@gmail.com>
2023-06-17 13:06:34 +0300

    feat: add func to cli:args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
+// args.js - implement function that parses command line arguments
+//  (given in format --propName value --prop2Name value2, you don't need to validate it)
+//   and prints them to the console in the format propName is value, prop2Name is value2
+
 const parseArgs = () => {
-    // Write your code here 
+    const argumentsPassed = process.argv.slice(2)
+    const result = [];
+
+    argumentsPassed.forEach((arg) => {
+        if (arg.includes('--')) {
+            const index = argumentsPassed.indexOf(arg)
+            result.push([arg.replace('--', ''), argumentsPassed[index + 1]])
+        }
+    })
+
+    result.forEach(item => {
+        console.log(item.join(' is '))
+    })
 };
 
 parseArgs();
