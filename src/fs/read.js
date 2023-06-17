e578637d31ecb633eb9c02d9a2694baac1fec781
Kolko Valentin <valiant9191@gmail.com>
2023-06-17 10:57:07 +0300

    feat: add func to read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import { readFile } from 'fs/promises';
+import path from 'path'
+
 const read = async () => {
-    // Write your code here 
+    const absolutePath = path.dirname(process.argv[1])
+    const filePath = `${absolutePath}/files/fileToRead.txt`
+    const errorMessage = 'FS operation failed'
+
+    try {
+        await readFile(filePath, { encoding: 'utf8' })
+            .then((data) => {
+                console.log(data)
+            })
+    } catch (err) {
+        console.error(errorMessage)
+    }
 };
 
 await read();
