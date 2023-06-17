3abecbd5ca7591a13be2c3c9cb1be37e6e5e661d
Kolko Valentin <valiant9191@gmail.com>
2023-06-17 13:29:57 +0300

    feat: add func to cli:env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    const argumentsPassed = process.env
+    const result = [];
+
+    for (const arg in argumentsPassed) {
+        if (arg.includes('RSS_')) {
+            result.push([arg, argumentsPassed[arg]])
+        }
+    }
+
+    console.log(result.map(item => item.join('=')).join('; '))
 };
 
 parseEnv();
