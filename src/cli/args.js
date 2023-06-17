690e3023a4ca8ca51097e0bbd99e163b5210e343
Kolko Valentin <valiant9191@gmail.com>
2023-06-17 13:07:17 +0300

    fix: clean commited

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,7 +1,3 @@
-// args.js - implement function that parses command line arguments
-//  (given in format --propName value --prop2Name value2, you don't need to validate it)
-//   and prints them to the console in the format propName is value, prop2Name is value2
-
 const parseArgs = () => {
     const argumentsPassed = process.argv.slice(2)
     const result = [];
