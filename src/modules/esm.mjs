4fa638378efe94b786ee739fc798feb221388634
Kolko Valentin <valiant9191@gmail.com>
2023-06-18 09:57:44 +0300

    fix: clean & refactor module

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,19 +1,14 @@
-// You should refactor file (you can add additional imports if needed)
-// cjsToEsm.cjs - rewrite it to it's equivalent in ECMAScript notation (and rename it to esm.mjs)
-
-
 import path from 'path';
 import { release, version } from 'os';
 import { createServer as createServerHttp } from 'http';
 
-
 import { createRequire } from "module"
-
 
 const require = createRequire(import.meta.url);
 const fileA = require('./files/a.json')
 const fileB = require('./files/b.json')
-// require('./files/c');
+
+import './files/c.js';
 
 const random = Math.random();
 
