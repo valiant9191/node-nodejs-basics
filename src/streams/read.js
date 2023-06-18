2937856bb3ac472b9142dcf83ec6be52e384f414
Kolko Valentin <valiant9191@gmail.com>
2023-06-18 11:25:43 +0300

    feat: add func to streams:read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import path from 'path'
+import { createReadStream } from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    const absolutePath = path.dirname(process.argv[1])
+    const pathToFile = `${absolutePath}/files/fileToRead.txt`
+    const stream = createReadStream(pathToFile, 'utf-8')
+    // stream.on('open', () => process.stdout.write('start reading file \n'))
+    stream.on('data', (data) => process.stdout.write(data))
+    // stream.on('end', () => process.stdout.write('\nend of reading'))
 };
 
 await read();
