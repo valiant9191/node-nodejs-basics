3f9e386930012c6181f550372a9e70a72820b762
Kolko Valentin <valiant9191@gmail.com>
2023-06-18 11:11:43 +0300

    feat: add func to hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from 'crypto'
+import { readFile } from 'fs/promises';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const absolutePath = path.dirname(process.argv[1])
+    const filePath = `${absolutePath}/files/fileToCalculateHashFor.txt`
+    try {
+        const data = await readFile(filePath, { encoding: 'utf-8' })
+
+        const hash = createHash('sha256')
+        const resultHash = hash.update(data)
+        const resultHashAsHex = resultHash.digest('hex')
+
+        return console.log(resultHashAsHex)
+    } catch (err) {
+        console.log('smth gone wrong... ' + '\n' + err.message)
+    }
 };
 
 await calculateHash();
