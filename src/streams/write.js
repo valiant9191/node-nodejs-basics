4477bce970f77c91e9311928d0a6bb7fbaacf01e
Kolko Valentin <valiant9191@gmail.com>
2023-06-18 11:49:48 +0300

    feat: add func to stream:write task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import path from 'path'
+import { createWriteStream } from 'fs';
+import { createInterface } from 'readline';
+
 const write = async () => {
-    // Write your code here 
+    const absolutePath = path.dirname(process.argv[1])
+    const pathToFile = `${absolutePath}/files/fileToWrite.txt`
+
+    const stream = createWriteStream(pathToFile, 'utf-8')
+    const readline = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    })
+    readline.on('line', (data) => { stream.write(data) })
 };
 
 await write();
