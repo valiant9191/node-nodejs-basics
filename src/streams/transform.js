1b951f840630aa53f88f5a21d74fb2193f70bdf3
Kolko Valentin <valiant9191@gmail.com>
2023-06-19 23:28:42 +0300

    feat: not finished transform + workthreads(wt)

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+// transform.js - implement function that reads data from process.stdin,
+//  reverses text using Transform Stream and then writes it into process.stdout
+
+
 const transform = async () => {
-    // Write your code here 
+
+    const reverseStream = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversedText = chunk.toString().split('').reverse().join('');
+            this.push(reversedText);
+            callback();
+        }
+    });
+
+    process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,22 @@
+// worker.js - extend given function to work with data received
+//  from main thread and implement function
+//   which sends result of the computation to the main thread
+import { Worker } from 'worker_threads'
+import path from 'path';
+
+
 const performCalculations = async () => {
-    // Write your code here
+    const absolutePath = path.dirname(process.argv[1])
+
+    const worker = new Worker(`${absolutePath}/worker.js`, { workerData: 10 });
+
+    worker.on('message', (result) => {
+        console.log('Result:', result);
+    });
+
+    worker.on('error', (error) => {
+        console.error('Worker Error:', error);
+    });
 };
 
 await performCalculations();
