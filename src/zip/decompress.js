09847c56605c3c5fdfaf2aaa634e458bb5278be3
Kolko Valentin <valiant9191@gmail.com>
2023-06-19 23:26:52 +0300

    feat: add func to decompress task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import path from 'path';
+import { createUnzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'fs'
+import { pipeline } from 'node:stream';
+
 const decompress = async () => {
-    // Write your code here 
+    const absolutePath = path.dirname(process.argv[1])
+    const filePath = `${absolutePath}/files/archive.gz`
+    const outputPath = `${absolutePath}/files/fileToCompress.txt`
+
+    try {
+        const gzip = createUnzip();
+        const source = createReadStream(filePath);
+        const destination = createWriteStream(outputPath);
+        pipeline(source, gzip, destination, (err) => {
+            if (err) { console.log(err) } else {
+                console.log('success')
+            }
+        });
+    } catch (err) {
+        console.error('An error occurred:', err);
+    }
 };
 
 await decompress();
