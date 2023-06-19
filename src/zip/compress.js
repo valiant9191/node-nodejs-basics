3a284b2032dda4a90de43225cbbcaac831f17b3c
Kolko Valentin <valiant9191@gmail.com>
2023-06-19 23:19:19 +0300

    feat: add func to compress task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import path from 'path';
+import { createGzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'fs'
+import { pipeline } from 'node:stream';
+
 const compress = async () => {
-    // Write your code here 
+    const absolutePath = path.dirname(process.argv[1])
+    const pathToFile = `${absolutePath}/files/fileToCompress.txt`
+    const outputPath = `${absolutePath}/files/archive.gz`
+
+    try {
+        const gzip = createGzip();
+        const source = createReadStream(pathToFile);
+        const destination = createWriteStream(outputPath);
+        pipeline(source, gzip, destination, (err) => {
+            if (err) { console.log(err) } else {
+                console.log('success')
+            }
+        });
+    } catch (err) {
+        console.error('An error occurred:', err);
+    }
 };
 
 await compress();
